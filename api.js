44ecea46610f6cbcb45c838eda3902814c092eab
eslamheshamm <esl2mhesham@gmail.com>
2021-01-18 10:59:38 +0200

    Creating api to read the data

---
+++ api.js
@@ -0,0 +1,33 @@
+import { readFileSync, readdirSync } from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+
+const postDirectory = join(process.cwd(), "blog");
+
+export function getAllSlugs() {
+	return readdirSync(postDirectory)
+		.filter((path) => path.endsWith(".md"))
+		.map((path) => path.replace(/\.md$/, ""));
+}
+
+export function getPostBySlug(slug) {
+	const path = join(postDirectory, `${slug}.md`);
+	const text = readFileSync(path, "utf-8");
+	const { data, content } = matter(text);
+	return {
+		frontmatter: data,
+		html: content,
+		slug,
+	};
+}
+
+export function getAllPosts() {
+	const posts = getAllSlugs()
+		.map((slug) => getPostBySlug(slug))
+		.sort((post1, post2) =>
+			new Date(post1.frontmatter.date) > new Date(post2.frontmatter.date)
+				? 1
+				: -1
+		);
+	return posts;
+}
